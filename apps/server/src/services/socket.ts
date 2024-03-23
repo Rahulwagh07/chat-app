cce98ecd7e9900ee106bf40c5e7a27c0fe4d0c64
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-03-24 03:33:17 +0530

    added kafka and postgresql

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,67 @@
+// ref:https://github.com/piyushgarg-dev/Scaleable-WebSockets/tree/main
+
+import { Server } from "socket.io";
+import Redis from "ioredis";
+import prismaClient from "./prisma";
+import { produceMessage } from "./kafka";
+import {config as configDotenv} from "dotenv"
+
+configDotenv();
+
+const pub = new Redis({
+    host: " ",
+    port: 20712,
+    username: "default",
+    password: "",
+});
+
+const sub = new Redis({
+    host: "",
+    port: 20712,
+    username: "default",
+    password: "",
+});
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init Socket Service...");
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    sub.subscribe("MESSAGES");
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("Init Socket Listeners...");
+
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Rec.", message);
+        // publish this message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
+
+    sub.on("message", async (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("new message from redis", message);
+        io.emit("message", message);
+        await produceMessage(message);
+        console.log("Message Produced to Kafka Broker");
+      }
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
